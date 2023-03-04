422f914ac5b84467426635f9a97ca089385d1307
Caio Cesar <ahcaio@hotmail.com>
2023-03-04 13:57:57 -0300

    APIKey fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const error404 = document.querySelector('.not-found');
 
 search.addEventListener('click', () => {
 
-    const APIKey = '728b0ee6df5687559812bd3169ad77b7';
+    const APIKey = '7e8ceaea20f6ffba42c9e95827db3b58';
     const city = document.querySelector('.search-box input').value;
 
     if (city === '')
